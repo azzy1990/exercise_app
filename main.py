daee50bf78be25bcaf0541fea48eeafa534c6bea
Michal Boduch <40508692@live.napier.ac.uk>
2022-03-14 21:17:02 +0100

    panda import added

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
 This is the start of the exercise-app project.
-
 Below is a simple 'Hello World' script for testing.
-
 This will validate that Pycharm is working locally on your device
 and Github our version control system is syncing properly.
-
 The console bellow should print 'Hi, Group!'
 """
+#  required  packages to implement this part:  pip  pandas  xlrd  openpyxl
+#  to install  >  Ctrl + Alt + S   >  (+)  to ADD PACKAGES
+import pandas as pd
 
 def print_hi(name):
     print(f'Hi, {name}')
@@ -15,8 +15,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Group!')
 
-if __name__ == '__main__':
-    print_hi('Group!')
-
-    if __name__ == '__main__':
-        print_hi('Group!')
